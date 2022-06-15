b8ca99911bc541a5e901f6c54e9a8779120dee70
advancedcat <wshx1938@163.com>
2022-06-15 17:43:39 +0800

    hash router

--- src/app.config.js
+++ src/app.config.js
@@ -1,9 +1,9 @@
 export default defineAppConfig({
   pages: [
+    'pages/hash-router/index',
     'pages/browser-router/index',
     'pages/index/index',
     'pages/history/index',
-    'pages/hash-router/index',
   ],
   window: {
     backgroundTextStyle: 'light',
